1a31abd284ed3e5415897d70096a5b11d493b0d8
Patricio Céspedes <patricio.isaiascv@gmail.com>
2026-09-06 22:29:14 -0300

    feat: mejoras en el panel admin, traduccion de datatables y logo en login

--- static/js/usuarios.js
+++ static/js/usuarios.js
@@ -276,8 +276,27 @@ function cargarTablaUsuariosAdmin() {
         $("#tablaUsuarios").DataTable().destroy();
     }
     $("#tablaUsuarios").DataTable({
-        language: { url: "https://cdn.datatables.net/plug-ins/1.13.11/i18n/es-ES.json" },
-        order: []
+        language: {
+            "decimal": "",
+            "emptyTable": "No hay información",
+            "info": "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
+            "infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
+            "infoFiltered": "(Filtrado de _MAX_ total entradas)",
+            "infoPostFix": "",
+            "thousands": ",",
+            "lengthMenu": "Mostrar _MENU_ Entradas",
+            "loadingRecords": "Cargando...",
+            "processing": "Procesando...",
+            "search": "Buscar:",
+            "zeroRecords": "Sin resultados encontrados",
+            "paginate": {
+                "first": "Primero",
+                "last": "Ultimo",
+                "next": "Siguiente",
+                "previous": "Anterior"
+            }
+        },
+        pageLength: 5,
     });
 }
 
